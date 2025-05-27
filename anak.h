586e082dab26cbe785d78f4532507d2e30c6a129
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-27 20:07:54 +0700

    Memasukkan class anak dan ibu dari file hpp

---
+++ anak.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+    public:
+    string nama;
+    anak(string pNama) : nama (pNama){
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+
+    ~anak(){
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+
+#endif
